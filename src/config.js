3f9abbdb593f9bc0bf4473ad2a5277f04a33910c
IsmaelPSY <ismaelpsy.12@gmail.com>
2022-07-17 14:07:24 -0500

    App configuration

---
+++ src/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+  port : process.env.PORT,
+  db: {
+    development: {
+        username: process.env.DB_DEV_USERNAME,
+        password: process.env.DB_DEV_PASSWORD,
+        database: process.env.DB_DEV_DATABASE,
+        dialect: 'postgres',
+    },
+    test: {
+        username: process.env.DB_TEST_USERNAME,
+        password: process.env.DB_TEST_PASSWORD,
+        database: process.env.DB_TEST_DATABASE,
+        dialect: 'postgres',
+    },
+    production: {
+        url: process.env.DATABASE_URL
+    },
+},
+}
