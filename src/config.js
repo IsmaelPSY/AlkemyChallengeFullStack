6b924241b52c2203af10e8018091a8c78da5245f
IsmaelPSY <ismaelpsy.12@gmail.com>
2022-07-17 19:00:21 -0500

    Setting Utils for encrypting passwords, session authentication, && handle promesis

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port : process.env.PORT,
+  jwtSecret : process.env.JWT_SECRET,
   db: {
     development: {
         username: process.env.DB_DEV_USERNAME,
